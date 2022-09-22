6639f7ce6a7af29bfb3144c3fa97c1c90cf48d72
satinder <satindersingh472@gmail.com>
2022-09-22 17:43:43 -0600

    finished

--- data_types.py
+++ data_types.py
@@ -1,7 +1,14 @@
 string_one = "this is the first string with single quotes"
 string_two = "this is the second string with double quotes test"
+list_numbers = [11,10,33,54,44,5454,34,66,433,677,5,534,4453,454556,2,5,0,]
 
 if (string_one.endswith("test") or string_two.endswith('test')):
     print('found test')
 else:
     print('no test')
+
+for each_number in list_numbers:
+    if(each_number > 10):
+        print(each_number, ': Large')
+    elif(each_number <= 10):
+        print(each_number,':Not Large')
